fb2bd64d4acdb9c4ee6e2445d20cfd86e9d5b210
jyaba <bishalnepali4@gmail.com>
2023-05-08 16:28:08 +0545

    Added the ip

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-def add(a, b):
-    return a + b
+import requests
+from datetime import datetime
+response = requests.get("https://www.httpbin.org/ip")
+json_response = response.json()
+origin = json_response.get("origin")
+
+
+# Appending to ip.txt
+with open("ip.txt", "a") as file:
+    file.write(origin + "\t" + str(datetime.now()) + "\n")
+
